fa43d79e88f6c327a6cceb2b1ccaeb591f6a1d9d
vgarg2005 <gargm4932@gmail.com>
2026-02-06 14:59:35 +0530

    New files added

--- latest/src/components/Header.jsx
+++ latest/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import './Header.css'
 
-export default function Header(props) 
+export default function Header({title}) 
 {
   return (
     <header className='navigation'>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     <nav>
         <a href="#">Home </a>
         <a href="#">Address </a>
