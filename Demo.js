0af0d55fb8cf955974bbfac673ff9cba026ac317
vgarg2005 <gargm4932@gmail.com>
2026-02-16 15:02:18 +0530

    Added new files

--- Demo.js
+++ Demo.js
@@ -27,7 +27,7 @@ do {
 //let arr = [1, 2, 3];
 //arr.forEach((element)=>console.log(element));
 
-function greet() {
+/*function greet() {
     console.log("Hello");
 }
 greet();
@@ -35,4 +35,32 @@ greet();
 function add(a, b) { // Simple functions
     console.log(a + b);
 }
-add(10, 5);
+add(10, 5);*/
+
+// Call Back Basics
+function greet() {
+  console.log("Welcome");
+}
+
+function execute(fn) {
+  fn();
+}
+execute(greet);
+
+//Map method deos not changes its arrays oroginal values rather it generates a new array.
+const number = [1, 2, 3, 4];
+let newNumber = number.map(function(n){
+  return n * 2;
+})
+console.log(newNumber);
+
+let newNum = number.map(n=>n*3);
+console.log(newNum);
+
+//Filter in array
+const num = [1, 4, 6, 8];
+const newNumb = num.filter(n=>n%2==0);
+console.log(newNumb);
+
+const sum = [...num, 5];
+console.log(sum);
---
+++ EventApp/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import EventToDo from './EventToDo'
+
+function App() {
+  return (
+    <EventToDo />
+  )
+}
+
+export default App
